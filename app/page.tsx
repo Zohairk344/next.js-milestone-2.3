ff833524c6097fa786bd2713800c4e495bd6cba9
zohairk344 <zohairm344@gmail.com>
2024-11-08 19:21:42 +0500

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,70 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-black text-white">
+      <header className="bg-slate-600 flex flex-col justify-center items-center gap-3">    
+        <h1 className="text-white text-4xl mt-10 my-10 font-bold ">Welcome to My IT Portfolio</h1>
+    </header>
+    
+    <section className="">
+        <div className="flex flex-col justify-center items-center">
+            <div className="h-[560px] w-[900px] bg-image rounded-xl mt-7 mb-10">
+            <Image src="/images/governer-house.jpg" alt="Resume" width={150} height={150} className="flex justify-center items-center mt-20 ml-96 mb-20"/>
+            <h1 className="flex justify-center items-center text-5xl font-semibold">Hello, My Name Is Zohair Khan</h1>
+            <p className="flex justify-center items-center mt-10 text-2xl">I’m currently completing my A levels </p>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+</section>
+
+
+    <section className="flex flex-col justify-center items-center" >
+        <div className="flex flex-col  items-center bg-white text-black h-[500px] w-[900px] rounded-xl">
+        <h2 className="text-2xl font-semibold mt-10 mb-14">Projects</h2>
+            <div className="">
+                <h3 className="flex flex-col justify-center items-center text-2xl font-semibold">Github Links</h3>
+                <p className="flex flex-col justify-center items-center mt-6 text-lg mb-8">Github Links Of The Projects I have Worked On</p>
+                <Link href="https://github.com/Zohairk344/GIAIC-100-day-coding-challenge" className="mx-2 font-semibold text-white bg-emerald-500 rounded-lg p-3">100 Days Coding Challenge</Link>
+                <Link href="https://github.com/Zohairk344/2024-Hackathon-milestone1" className="mx-2 font-semibold text-white bg-emerald-500 rounded-lg p-3">Hackathon Milestone 1</Link>
+                <Link href="https://github.com/Zohairk344/2024-Hackathon-milestone3" className="mx-2 font-semibold text-white bg-emerald-500 rounded-lg p-3">Hackathon Milestone 3</Link>
+                <h3 className="flex flex-col justify-center items-center text-2xl font-semibold mt-10 mb-9">Vercel Links</h3>
+                <Link href="https://2024-hackathon-ruby.vercel.app/" className="mx-5 ml-36 font-semibold text-white bg-emerald-500 rounded-lg p-3 ">Hackathon Milestone 1</Link>
+                <Link href="https://2024-hackathon-milestone3.vercel.app/" className="mx-5  font-semibold text-white bg-emerald-500 rounded-lg p-3">Hackathon Milestone 3</Link>
+            </div>
+        </div>
+    </section>
+    
+    
+    <section className="flex flex-col justify-center items-center mt-9">
+        <div className="flex flex-col justify-center bg-white text-black h-[250px] w-[900px] rounded-xl ">
+            <ul className="m-10 text-lg">
+                <li className="">Typescript</li>
+                <li className="mt-4"> HTML and CSS</li>
+                <li className="mt-4"> little bit LUA</li>
+                <li className="mt-4"> Learning Next.JS</li>
+            </ul>
+        </div>
+    </section>
+    
+    <section className="flex flex-col justify-center items-center mt-9">
+        <div className="flex flex-col bg-white text-black h-[250px] w-[900px] rounded-xl">
+            <h2 className="flex flex-col items-center mt-7 text-3xl font-semibold">Contact Me</h2>
+            <p className="mt-6 text-lg m-5">Email: zohairm344@gmail.com</p>
+            <p className="text-lg ml-5">Phone: 03353532659</p>
+        
+            <Link href="linkedin.com/in/zohair-khan-6b1716250" target="_blank">
+                <Image src="/images/images.png" alt="LinkedIn" width={50} height={50} className="m-5"/>
+            </Link>        
+        </div>
+
+    </section>
+
+    <footer>
+        <div className="bg-slate-600 p-9 mt-10">
+            <p className="flex flex-col justify-center items-center text-lg">&copy; 2024 My Portfolio</p>
+        </div>
+    </footer>
     </div>
   );
 }
