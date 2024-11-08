5b4b9ab4a06db91e194e7ac8da0b512a7f788d2d
zohairk344 <zohairm344@gmail.com>
2024-11-08 20:23:08 +0500

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { Roboto_Serif } from "next/font/google";
 import "./globals.css";
 
 const geistSans = localFont({
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import Header from "@/components/header";
 
 export default function Home() {
